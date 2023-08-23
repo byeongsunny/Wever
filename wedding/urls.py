8f35c8d3fd3dfaf1788ff8977dde4a6acc8904c8
Sungho Choi <starhochoi@gmail.com>
2023-08-23 09:09:46 +0900

    추가한 페이지들 url 연결

--- wedding/urls.py
+++ wedding/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path("res/", views.reservation_page, name="reservation_page"),
     path("non/", views.Nonhyeon_page, name="non_page"),
     path("sam/", views.Samsung_page, name="sam_page"),
+    path("chu/", views.Samsung_page, name="chu_page"),
+    path("sin/", views.Samsung_page, name="sin_page"),
 ]
