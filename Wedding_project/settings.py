196e9cec443b82bdb425705ba06e37aafb9de3cc
DESKTOP-BMF681M\user <khb3254@naver.com>
2023-08-24 09:44:00 +0900

    db 연동
    django와 mysql 연동
    settings.py에 db정보 입력

--- Wedding_project/settings.py
+++ Wedding_project/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = "Wedding_project.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "wedding",
+        "USER": "root",
+        "PASSWORD": "0000",
+        "HOST": "localhost",
+        "PORT": "3307",
     }
 }
 
